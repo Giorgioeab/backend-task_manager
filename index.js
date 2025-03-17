e257075370bd5f15fbc6d5567b7b4b0c7fc252c5
Giorgioeab <giorgioeabarbosa@gmail.com>
2025-03-17 03:46:48 -0300

    feat: add route to get task by id

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const task = await TaskModel.findById(taskId);
+        if (!task) {
+            return res.status(404).send("Tarefa não encontrada");
+        }
+
+        return res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
